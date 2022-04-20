e68bf5d126de7b1e512979acdd0a926f156a84ba
Kurt Maglalang <kbm126@live.com>
2022-04-20 14:26:51 -0700

    refactored daylist and item for passing set

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  return (
-    <ul>
-      {props.days.map((e) => (
-        <DayListItem
-          key={e.id}
-          name={e.name}
-          spots={e.spots}
-          selected={e.name === props.day}
-          setDay={props.setDay}
-        />
-      ))}
-    </ul>
-  );
+  const days = props.days.map((e) => (
+    <DayListItem
+      key={e.id}
+      name={e.name}
+      spots={e.spots}
+      selected={e.name === props.day}
+      setDay={() => {
+        props.setDay(e.name);
+      }}
+    />
+  ));
+
+  return <ul>{days}</ul>;
 }
