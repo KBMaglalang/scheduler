bc3f8718d208520d02d9b599bdefbe626fce3dc2
Kurt Maglalang <kbm126@live.com>
2022-04-19 17:25:36 -0700

    implemented test, format text depending on spots

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -9,6 +9,18 @@ export default function DayListItem(props) {
     { "day-list__item--full": !props.spots }
   );
 
+  const formatSpots = () => {
+    if (!props.spots) {
+      return "no spots remaining";
+    }
+
+    if (props.spots === 1) {
+      return "1 spot remaining";
+    }
+
+    return `${props.spots} spots remaining`;
+  };
+
   return (
     <li
       className={dayClass}
@@ -17,7 +29,7 @@ export default function DayListItem(props) {
       }}
     >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
