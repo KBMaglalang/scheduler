e6f8390d1693432316b021cb1f5079dc860d3ed2
Kurt Maglalang <kbm126@live.com>
2022-04-20 15:36:53 -0700

    show form setup

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 
 
@@ -121,35 +122,35 @@ storiesOf("InterviewerListItem", module)
 
 
 
-  const interviewers = [
-    { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-    { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-    { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-    { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-    { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-  ];
-  
-  storiesOf("InterviewerList", module)
-  .addParameters({
-    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
-  })
-  .add("Initial", () => (
-    <InterviewerList
-      interviewers={interviewers}
-    />
-  ))
-  .add("Selected", () => (
-    <InterviewerList
-      interviewers={interviewers}
-      value={3}
-    />
-  ))
-  .add("Clickable", () => (
-    <InterviewerList
-      interviewers={interviewers}
-      onChange={action("setInterviewer")}
-    />
-  ));
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
+
+storiesOf("InterviewerList", module)
+.addParameters({
+  backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+})
+.add("Initial", () => (
+  <InterviewerList
+    interviewers={interviewers}
+  />
+))
+.add("Selected", () => (
+  <InterviewerList
+    interviewers={interviewers}
+    value={3}
+  />
+))
+.add("Clickable", () => (
+  <InterviewerList
+    interviewers={interviewers}
+    onChange={action("setInterviewer")}
+  />
+));
 
 
 
@@ -163,3 +164,11 @@ storiesOf("Appointment", module)
 .add("Appointment with Time", () => <Appointment time="12pm"/>)
 .add("Header", () => <Header time="12pm"/>)
 .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+.add("Show", () => (
+  <Show 
+    student='Lydia Miller-Jones' 
+    interviewer={interviewer} 
+    onEdit={action("onEdit")} 
+    onDelete={action("onDelete")}
+  />
+))
