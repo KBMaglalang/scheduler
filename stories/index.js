78edbf3cd82106bbb1fe3108fa791548b63b0d2d
Kurt Maglalang <kbm126@live.com>
2022-04-20 15:51:41 -0700

    status component created

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,8 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 
 
@@ -180,3 +182,4 @@ storiesOf("Appointment", module)
     onCancel={action("onCancel")}
   />
 ))
+.add("Status", () => <Status message="Deleting" />)
