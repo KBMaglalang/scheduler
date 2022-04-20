f0baa2e26dac3ce015c14def6da265183d1434f7
Kurt Maglalang <kbm126@live.com>
2022-04-20 15:55:44 -0700

    error component created

--- stories/index.js
+++ stories/index.js
@@ -183,3 +183,9 @@ storiesOf("Appointment", module)
   />
 ))
 .add("Status", () => <Status message="Deleting" />)
+.add("Error", () => (
+  <Error 
+    message='Could not delete appointment.'
+    onClose={action("onClose")}
+  />
+))
