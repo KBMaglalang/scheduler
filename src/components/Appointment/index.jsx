b78c56be64e1369da0ad4d5afce01183687966bb
Kurt Maglalang <kbm126@live.com>
2022-04-20 17:30:50 -0700

    implemented components in appointment

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
-  const formatAppointment = () => {
-    if (!props.time) {
-      return "No Appointments";
-    }
-
-    return `Appointment at ${props.time}`;
-  };
-
-  return <article className="appointment">{formatAppointment()}</article>;
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }
