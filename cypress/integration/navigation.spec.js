44c0bd79079cfed6e9042c607a861dff1b1c3c20
Kurt Maglalang <kbm126@live.com>
2022-04-27 15:12:22 -0700

    cypress appointments testing

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,16 +1,10 @@
-const { describe } = require("eslint/lib/testers/rule-tester");
-
 describe("Navigation", () => {
   it("should visit root", () => {
-    // eslint-disable-next-line no-undef
     cy.visit("/");
   });
 
   it("should navigate to Tuesday", () => {
-    // eslint-disable-next-line no-undef
     cy.visit("/");
-
-    // eslint-disable-next-line no-undef
     cy.contains("li", "Tuesday")
       .click()
       .should("have.class", "day-list__item--selected");
