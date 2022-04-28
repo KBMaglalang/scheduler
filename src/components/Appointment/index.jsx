e0c7987be3733a3e7240e0e5492b0de37009a387
Kurt Maglalang <kbm126@live.com>
2022-04-28 12:50:52 -0700

    websocket communication works

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
@@ -45,12 +45,23 @@ export default function Appointment(props) {
       .catch(() => transition(ERROR_DELETE, true));
   };
 
+  useEffect(() => {
+    if (props.interview && mode === EMPTY) {
+      transition(SHOW);
+      return;
+    }
+    if (!props.interview && mode === SHOW) {
+      transition(EMPTY);
+      return;
+    }
+  }, [props.interview, transition, mode]);
+
   return (
     <article className="appointment">
       <Header time={props.time} />
 
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-      {mode === SHOW && (
+      {mode === SHOW && props.interview && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
