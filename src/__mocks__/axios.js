902c8d2be4470755dd6836c1093105866269afb3
Kurt Maglalang <kbm126@live.com>
2022-04-28 22:33:36 -0700

    application test 2 implemented

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -54,6 +54,10 @@ const fixtures = {
 };
 
 export default {
+  defaults: {
+    baseUrl: "",
+  },
+
   get: jest.fn((url) => {
     if (url === "/api/days") {
       return Promise.resolve({
@@ -76,6 +80,22 @@ export default {
         status: 200,
         statusText: "OK",
         data: fixtures.interviewers,
+      });
+    }
+  }),
+  put: jest.fn((url) => {
+    if (url.includes("/api/appointments")) {
+      return Promise.resolve({
+        status: 204,
+        statusText: "No Content",
+      });
+    }
+  }),
+  delete: jest.fn((url) => {
+    if (url.includes("/api/appointments")) {
+      return Promise.resolve({
+        status: 204,
+        statusText: "No Content",
       });
     }
   }),
